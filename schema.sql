313205788042e17902933489ce8cadc2e1f585b5
volosevych <vestyankas@gmail.com>
2020-10-12 17:54:10 -0600

    Made database. Tables created

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DELETE DATABASE IF EXISTS employee_tracker;
+
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    fist_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL
+);
