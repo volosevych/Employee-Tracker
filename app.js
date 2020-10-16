723087d24a9d5c43d05e816b7271c2c63b0af67e
volosevych <vestynkas@gmail.com>
2020-10-16 13:25:36 -0600

    added prompts to ask user add, view, update new employeer

--- app.js
+++ app.js
@@ -39,11 +39,39 @@ const db = new Database({
     database: "employee_tracker"
 });
 
-async function testFunc() {
+async function employeeSummary() {
     await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
         if (err) throw err;
         console.log(res);
+        runApp();
     });
 }
 
-testFunc();
+function runApp() {
+    inquirer
+    .prompt({
+        name: "mainmenu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all employeers",
+            "Add a new employee",
+            "Add a new role",
+            "Add a new Department"
+        ]
+    }).then( (res) => {
+        switch (res.mainmenu) {
+            case "View all employeers":
+                employeeSummary();
+                break;
+            case "Add a new employee":
+                break;
+            case "Add a new role":
+                break;
+            case "Add a new Department":
+                break;
+        }
+    });
+}
+
+runApp();
